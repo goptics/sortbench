d7427476d9f456b824a554b4cf449244d439c4c9
fahimfaisaal <fahimfaisaal.dev@gmail.com>
2026-05-16 10:15:06 +0600

    feat: replace shell sort with quicksort

--- sort.go
+++ sort.go
@@ -1,18 +1,26 @@
 package sortbench
 
 func Sort(nums []int) {
-	n := len(nums)
-	gap := n / 2
-	for gap > 0 {
-		for i := gap; i < n; i++ {
-			key := nums[i]
-			j := i
-			for j >= gap && nums[j-gap] > key {
-				nums[j] = nums[j-gap]
-				j -= gap
-			}
-			nums[j] = key
-		}
-		gap /= 2
+	quickSort(nums, 0, len(nums)-1)
+}
+
+func quickSort(nums []int, low, high int) {
+	if low < high {
+		pi := partition(nums, low, high)
+		quickSort(nums, low, pi-1)
+		quickSort(nums, pi+1, high)
 	}
+}
+
+func partition(nums []int, low, high int) int {
+	pivot := nums[high]
+	i := low
+	for j := low; j < high; j++ {
+		if nums[j] < pivot {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+	}
+	nums[i], nums[high] = nums[high], nums[i]
+	return i
 }
